e456d28b58b008552a506da75076fa7e166d1847
Vladimir Barentsev <Vladimir.Barentsev@waves.com>
2026-01-10 19:22:32 +0200

    Add masters results entry page

--- client/vite.config.js
+++ client/vite.config.js
@@ -30,6 +30,7 @@ export default defineConfig(({ command }) => ({
       input: {
         home: resolve(__dirname, 'home.html'),
         results_main: resolve(__dirname, 'results_main.html'),
+        result_masters: resolve(__dirname, 'result-masters.html'),
         'dolphin-masters': resolve(__dirname, 'dolphin-masters.html'),
         about: resolve(__dirname, 'about.html'),
       },
